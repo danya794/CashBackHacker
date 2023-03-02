fe2171afa6d9c6d44b73367510fe6f35941c9e54
Danil Kovalev <dannykvlv@gmail.com>
2023-03-02 11:24:41 +0300

    test added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -14,4 +14,22 @@ public class CashbackHackServiceTest {
         int actual = service.remain(amount);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testRemainAmount100() {
+        int amount = 100;
+        int expected = 900;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainAmount1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
 }
